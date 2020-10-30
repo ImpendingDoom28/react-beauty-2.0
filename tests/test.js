6b202120454b26f585b6897e09026f617fbefcca
Sergey Mikheev <mikheevs11@gmail.com>
2020-10-30 21:32:50 +0300

    Returned the React-Beauty Theme look + added some tests

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+function basicFunction(param1, param2) {
+	console.log(param1, param2);
+	return param2;
+}
+
+const arrowFunction = (param1, param2) => {
+	console.log(param1, param2);
+	return param1;
+}
+
+const CONSTANT = "YELLOW";
+
+const { data } = { data: "1" }
